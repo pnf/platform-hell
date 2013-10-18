4261e441c7c394f4c997d83bc548f2441de0b020
Peter Fraenkel <pnf@podsnap.com>
2013-10-18 11:26:52 -0400

    Fix date calculations, format graph prettily

--- src/mta_browser/routes/home.clj
+++ src/mta_browser/routes/home.clj
@@ -25,19 +25,53 @@
                          :day        day
                          :stop_id    stop_id}))
 
+(defn daysecs-to-millis
+  "Convert sequence of seconds since given datetime to millis since epoch"
+  [date secs]
+  (map #(dt/in-millis (dt/interval (dt/epoch)
+                                   (dt/plus date
+                                            (dt/seconds %))))
+       secs))
+
+(defn midnight-in-local [datestring]
+  "Get ISO date, datetime and day of week at midnight of date given in string, which is
+assumed to be in local zone."
+  (let [tz       (dt/default-time-zone)
+        fmt      (df/formatter "YYYY-MM-dd" tz)
+        t        (if (seq datestring)
+                   (df/parse fmt datestring)
+                   (dt/to-time-zone (dt/now) tz))
+        ds       (df/unparse fmt t)
+        dt       (df/parse fmt ds)
+        dw       (dt/day-of-week dt)]
+    [ds dt dw]))
+
+(defn nice-title [stop_id route_id]
+  (let [stop_nm   (:stop_name  (mc/find-one-as-map "stops" {:stop_id stop_id}))
+        dir      (if (= \S (last stop_id)) "Downtown" "Uptown")]
+    (str dir " " route_id " at " stop_nm)))
 
 (defn make-graph [date service_code stop_id route_id]
-  (let [t             (if-not (empty? date) (df/parse date) (dt/now))
-        date          (df/unparse (df/formatter "YYYY-MM-dd") t)
-        service_code  (cond (seq service_code)         service_code
-                            (= (dt/day-of-week t) 6)  "SAT"
-                            (= (dt/day-of-week t) 7)  "SUN"
-                            :else                     "WKD")
+  (let [tz            (dt/default-time-zone)
+        fmt           (df/formatter "YYYY-MM-dd" tz)
+        [ds dt dw]    (midnight-in-local date)
+        service_code  (cond (seq service_code) service_code
+                            (= dw 6)          "SAT"
+                            (= dw 7)          "SUN"
+                            :else              "WKD")
         sched         (get-sched stop_id route_id service_code)
-        rates         (get-rates stop_id route_id date)]
+        rates         (get-rates stop_id route_id ds)]
     (doto
-      (icharts/xy-plot (map :now sched) (map :rate sched))
-      (icharts/add-points (map :t_day rates) (map :rate rates)))))
+        (icharts/time-series-plot (daysecs-to-millis dt (map :now sched))
+                                  (map :rate sched)
+                       :title (nice-title stop_id route_id)
+                       :x-label "Time" 
+                       :y-label "Trains per hour"
+                       :legend true
+                       :series-label "Scheduled")
+      (icharts/add-points (daysecs-to-millis dt (map :t_day rates))
+                          (map :rate rates)
+                          :series-label "Actual"))))
 
 (defn show-graph [date service_code stop_id route_id]
   (cond
