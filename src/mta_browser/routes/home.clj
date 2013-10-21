d6882e0f93b738ccec7f4e4d561c787369505a97
Peter Fraenkel <pnf@podsnap.com>
2013-10-21 13:07:07 -0400

    Basic form functionality

--- src/mta_browser/routes/home.clj
+++ src/mta_browser/routes/home.clj
@@ -1,5 +1,5 @@
 (ns mta-browser.routes.home
-  (:use compojure.core)
+  (:use compojure.core monger.operators )
   (:require [mta-browser.views.layout :as layout]
             [mta-browser.util :as util]
              [monger.core :as mg]
@@ -11,9 +11,6 @@
             [clj-time.format :as df]
             [noir.response :as nr]))
 
-(defn home-page []
-  (layout/render
-    "home.html" {:content (util/md->html "/md/docs.md")}))
 
 (defn- get-sched [stop_id route_id service_code]
   (mc/find-maps "sched" {:route_id       route_id
@@ -46,10 +43,54 @@ assumed to be in local zone."
         dw       (dt/day-of-week dt)]
     [ds dt dw]))
 
+
+(defn throttled-fetch [cache millis fetcher & args]
+  (let [now      (System/currentTimeMillis)
+        then     (- now millis)]
+    (-> cache
+        (swap! (fn [{time :time :as orig}]
+                 (if (> time then)
+                   orig
+                   {:time now :data (apply fetcher args)})))
+        :data)))
+
+(def stop-name-cache (atom {:time 0}))
+(defn- fetch-stop-names []
+  (reduce #(assoc %1 (:stop_id %2) (:stop_name %2)) {}                           
+          (mc/find-maps "stops")))
+(defn get-stop-names [] 
+  (throttled-fetch stop-name-cache (* 7 24 3600 1000) fetch-stop-names))
+
 (defn nice-title [stop_id route_id]
-  (let [stop_nm   (:stop_name  (mc/find-one-as-map "stops" {:stop_id stop_id}))
-        dir      (if (= \S (last stop_id)) "Downtown" "Uptown")]
-    (str dir " " route_id " at " stop_nm)))
+  (let [stop_nm   ((get-stop-names) stop_id)
+        dir       (if (= \S (last stop_id)) "Downtown" "Uptown")]
+    (str stop_nm ": " dir " " route_id)))
+
+(def platform-compare (comparator (fn [rec1 rec2]
+                                    (neg? (compare (:title rec1)
+                                                   (:title rec2))))))
+(defn- fetch-platforms
+  "Returns list of maps like
+ {:station_name \"33 St\", :route_id \"5\", :dir \"N\", :stop \"632\", :stop_id \"632N\"}"
+  []
+  (let [one-week-ago    (-  (int (/ (System/currentTimeMillis) 1000)) (* 7 24 3600))
+        query           [{$match {:now {$gt one-week-ago}}}
+                         {$project {:platform {"$concat" ["$stop_id" ":" "$route_id"]}}}
+                         {$group {:_id "na" :platforms {$addToSet "$platform"}}}]
+        code->platform  (->> (mc/aggregate "rates" query) first :platforms
+                      ; ==> "132S:2", "632N:5", etc.
+                             (map (partial re-find #"((\S+)([NS])):(\S+)"))
+                             (map #(zipmap [:code :stop_id :station :dir :route_id] %))
+                             (map #(assoc %
+                                     :title (nice-title (:stop_id %) (:route_id %))
+                                     :station_name ((get-stop-names) (:stop_id %))))
+                             (reduce #(assoc %1 (:code %2) %2) {}))
+        sorted-platforms (sort platform-compare (vals code->platform))]
+    {:code-> code->platform :sorted sorted-platforms}))
+
+(def platform-cache (atom {:time 0}))
+(defn get-platforms []
+  (throttled-fetch platform-cache (* 7 24 3600 1000) fetch-platforms ))
 
 (defn make-graph [date service_code stop_id route_id]
   (let [tz            (dt/default-time-zone)
@@ -73,20 +114,26 @@ assumed to be in local zone."
                           (map :rate rates)
                           :series-label "Actual"))))
 
-(defn show-graph [date service_code stop_id route_id]
-  (cond
-   (or (empty? stop_id)
-       (empty? route_id))   "Format graph?date=YYYY-MM-DD&stop_id=NNND&route_id=R"
-    :else
-    (let [graph     (make-graph date service_code stop_id route_id)
-          os        (java.io.ByteArrayOutputStream.)
-          is        (do (icore/save graph os)
-                        (java.io.ByteArrayInputStream. 
-                         (.toByteArray os)))]
-      (nr/content-type "image/png" is))))
+(defn show-graph [date service_code platform]
+  (let [platform ((:code-> (get-platforms)) platform)]
+    (if-not platform
+      "Format graph?date=YYYY-MM-DD&platform=NNND:R"
+      (let [graph     (make-graph date service_code
+                                  (:stop_id platform)
+                                  (:route_id platform))
+            os        (java.io.ByteArrayOutputStream.)
+            is        (do (icore/save graph os)
+                          (java.io.ByteArrayInputStream. 
+                           (.toByteArray os)))]
+        (nr/content-type "image/png" is))
+      )))
+
+(defn home-page []
+  (layout/render
+   "home.html" {:platforms (:sorted (get-platforms))}))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/graph"
-       [date service_code stop_id route_id]
-       (show-graph date service_code stop_id route_id)))
+       [date service_code platform]
+       (show-graph date service_code platform)))
